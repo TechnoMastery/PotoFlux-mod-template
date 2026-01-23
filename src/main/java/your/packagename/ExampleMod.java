0f9b9e7bce8c083ed8529a44d878b7afe277ceca
Minheur2000 <adrien.rousselet@hotmail.com>
2026-01-23 18:55:25 +0100

    finished update

--- src/main/java/your/packagename/ExampleMod.java
+++ src/main/java/your/packagename/ExampleMod.java
@@ -5,7 +5,6 @@ import net.minheur.potoflux.loader.PotoFluxLoadingContext;
 import net.minheur.potoflux.loader.mod.Mod;
 import net.minheur.potoflux.loader.mod.ModEventBus;
 import net.minheur.potoflux.loader.mod.events.RegisterLangEvent;
-import net.minheur.potoflux.loader.mod.events.RegisterTabsEvent;
 import your.packagename.tabs.Tabs;
 import your.packagename.translations.ExampleModTranslations;
 
@@ -13,12 +12,9 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 
-@Mod(modId = ExampleMod.MOD_ID, version = "1.0", compatibleVersions = ExampleMod.COMPATIBLE_VERSIONS)
+@Mod(modId = ExampleMod.MOD_ID, version = "1.0", compatibleVersions = {"6.0"})
 public class ExampleMod {
     public static final String MOD_ID = "yourmodid";
-    public static final String[] COMPATIBLE_VERSIONS = new String[] {
-            "6.0"
-    };
 
     public ExampleMod() {
         ModEventBus modEventBus = PotoFluxLoadingContext.get().getModEventBus();
